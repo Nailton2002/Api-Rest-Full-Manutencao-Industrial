e6b554448da5a73010cea973cebc989a70628b19
Nailton2002 <jnmp25jnmp@gmail.com>
2023-06-15 20:47:50 -0300

    Criando o projeto

---
+++ src/main/java/com/manutencao/industrial/ManutencaoIndustrialApplication.java
@@ -0,0 +1,13 @@
+package com.manutencao.industrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManutencaoIndustrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManutencaoIndustrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manutencao/industrial/ManutencaoIndustrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manutencao.industrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManutencaoIndustrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
